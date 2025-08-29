a87921cbbb8d14076bf10e0b30fce8592fcaf040
Karen Barbé <info@karenbarbe.com>
2025-08-29 18:44:48 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
 
