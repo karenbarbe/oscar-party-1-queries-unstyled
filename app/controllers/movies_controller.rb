5aa182c6ddaa2075b1438f64dfc902d2fc2a9a13
Karen Barbé <info@karenbarbe.com>
2025-08-29 17:34:32 -0500

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,15 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    render({ :template => "movie_template/index"})
+    @movies = Movie.all.order({ :year => :desc, :title => :asc })
+    render({ :template => "movie_template/index" })
+  end
+
+  def show
+    path_id = params.fetch("path_id")
+    matching_movies = Movie.where({ :id => path_id })
+    @movie = matching_movies.first
+    director = Director.where({ :id => @movie.director_id }).first
+    @director_name = "#{director.first_name} #{director.last_name}"
+    render({ :template => "movie_template/show" })
   end
 end
